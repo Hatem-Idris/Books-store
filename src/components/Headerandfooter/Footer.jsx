1eac11aad3938c0e1db6379f4b3d48baae1f59ec
Hatem-Idris <hatem.h.idris@gmail.com>
2026-01-28 17:38:31 +0200

    Books store

--- src/components/Headerandfooter/Footer.jsx
+++ src/components/Headerandfooter/Footer.jsx
@@ -1,22 +1,51 @@
 import { MdOutlineBook } from "react-icons/md";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { PiYoutubeLogo } from "react-icons/pi";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaEarthAmericas } from "react-icons/fa6";
+
 export default function Footer() {
   return (
-    <footer className="bg-white/20 w-full py-6">
-      <div className="w-full flex container mx-auto justify-between bg-[#3B2F4A] py-20 ">
-        <nav className="flex w-full items-center gap-6 text-white font-[600] border-b-2 border-b-white/20">
-          <MdOutlineBook className="text-2xl" />
-          <span>Bookshop</span>
-          <span className="text-white/30">|</span>
-          <ul className="flex gap-10 ">
-            <li>Home</li>
-            <li>Books</li>
-            <li>About Us</li>
-          </ul>
-        </nav>
-        <div className="w-full flex justify-between">
-            <p>
-                
-            </p>
+    <footer className="w-full bg-[#3B2F4A]">
+      <div className="container mx-auto py-20 md:px-5 flex flex-col gap-8 text-white">
+        <div className="flex justify-between">
+          <nav className="flex font-[600]">
+            <div className="flex gap-6">
+              <MdOutlineBook className="text-2xl" />
+              <span>Bookshop</span>
+              <span className="text-white/30">|</span>
+              <ul className="flex gap-10">
+                <li>Home</li>
+                <li>Books</li>
+                <li>About Us</li>
+              </ul>
+            </div>
+          </nav>
+          <div className="flex gap-4">
+            <FaFacebook className="w-[24px] h-[24px]" />
+            <FaInstagram className="w-[24px] h-[24px]" />
+            <PiYoutubeLogo className="w-[24px] h-[24px]" />
+            <FaSquareXTwitter className="w-[24px] h-[24px]" />
+          </div>
+        </div>
+
+        <hr className="w-full border-t border-white/20" />
+        <div className="flex justify-between items-center text-sm text-white/70">
+          <p>Developed By EraaSoft All Copy Rights Reserved @2024</p>
+          <div className="flex items-center gap-4">
+            <FaEarthAmericas className="w-[24px] h-[24px]" />
+
+            <select
+              defaultValue="English"
+              className="select appearance-none bg-transparent border border-white/50 rounded-full"
+            >
+              <option disabled={true}>Pick a language</option>
+              <option className="text-black">English</option>
+              <option className="text-black">Arabic</option>
+              <option className="text-black">Italy</option>
+            </select>
+          </div>
         </div>
       </div>
     </footer>
